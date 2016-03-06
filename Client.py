13180b04d2da1d4708c10f99b48a4da8cafcf7e1
sundhar010 <sundhar.i14@iiits.in>
2016-03-06 21:14:49 +0530

    update

--- Client.py
+++ Client.py
@@ -1,27 +1,68 @@
+
+############################################################################################################################################
+#                                                       Client side of client server communication                                         #
+
+MYPORT = 8080                                          # port for client sever commuication 
+IP_REQUEST_PORT = 9000                                 # Port for requesting server for its ip                    
+IP_RECEVE_PORT = 7000                                  # port for receving the servers ip as respoce
+
+
+import os,sys
 import socket
 import thread
-def dsp(s):
-	while True:
-		data = s.recv(1024)
-		print "msg >" + str(data)
-		pass
-def msg(s):
-	while True:
-		message = raw_input(">>")
-		s.send(message)
-		if message == 'q':
-			s.close()
-			break
+
+
+def dspMsg(s):                                        #
+	while True:                                   #
+		data = s.recv(1024)                   #  function used by a thread to display the msg sent from the client
+		print ">>" + str(data)                #
+		pass                                  #
+
+
+
+def sendMsg(s):                                       #
+	while True:                                   #
+		message = raw_input("")               #
+		if message == "quit()":               #  function used by a thred to send the msg to the server
+			s.close()                     #
+			os._exit(1)                   #
+		s.send(message)                       #
+		pass                                  #
+
+
+
+def getserver( ):
+	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)         #
+	s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)       #
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)     #
+	s.sendto("who is the server?", ('<broadcast>', IP_REQUEST_PORT))#
+	s.close()                                                   #
+	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)         #
+	s.bind(("",IP_RECEVE_PORT))                                 #this function broadcasts that it wants the ip of server , receves the 
+	data = ""                                                   #ip when the server replyes and returns it .
+	while not len(data):                                        #
+		data, addr = s.recvfrom(1024)                       #
+		pass                                                #
+	print data                                                  #
+	sip = addr[0]                                               #
+	print sip                                                   #
+	s.close()                                                   #
+	return sip                                                  #
+
+
+
+def main():
+	
+	port = MYPORT
+	host = getserver()
+	s = socket.socket()
+	s.connect((host,MYPORT))
+	thread.start_new_thread(dspMsg,(s,)) #thread used for desplaying the msgs
+	thread.start_new_thread(sendMsg,(s,))#thread used for sending msgs
+	while 1:
 		pass
 
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect(('127.0.0.1',12345))
-print "my ip addres " + str(s.getsockname()[0])
-host = '127.0.0.1'
-port = 3300
-s = socket.socket()
-s.connect((host,port))
-thread.start_new_thread(dsp,(s,))
-thread.start_new_thread(msg,(s,))
-while 1:
-	pass
+
+
+if __name__ == '__main__':
+	main()
--- Server.py
+++ Server.py
@@ -1,37 +1,78 @@
+############################################################################################################################################
+#                                                       Server side of client server communication                                         #
+
+
+IP_REQUEST_PORT = 9000                                           # port used for accepting client's request for server's ip
+IP_SEND_PORT = 7000	                                         # port used for sending servers ip
+MYPORT = 8080                                                    # port for server client communication
+NoClients =  3                                                   # number of clients which server accept
+lis=[]                                                           # List of all client's sockets which are connected to server
+
+import sys,os
 import socket 
-import sys
 import thread
-def dsp(c):
-	print "dsp"
+
+
+def sendip():
 	while True:
-		data = c.recv(1024)
-		if not data:
-			break
-		print "Him >>"+ str(data)
-		pass
-	c.close()
-def msg(c):
-	print "msg"
-	while True:
-		data = raw_input("YOU >>")
-		if data == 'q':
-			c.close()
-			quit()
-		c.send(data)
-		pass
+		s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)        #  
+		s.bind(("",IP_REQUEST_PORT))                               #  
+		data = ""                                                  #
+		while not len(data):                                       #  Server Waiting For the request from the client for it's IP
+			data, addr = s.recvfrom(1024)                      #
+		print data                                                 #
+		s.close()                                                  #
+
+
+		s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)        #
+		s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)	   #
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)    #  server replying the client with its ip
+		s.sendto("I am the Server", ('<broadcast>', IP_SEND_PORT)) #
+		s.close()                                                  #
+		pass                                                       #
+
+
+def dspMsg(c):                                                             #
+	while True:                                                        #
+		data = c.recv(1024)                                        #
+		if not data:                                               #  function used by a thread to display client's msgs
+			break                                              #  
+		print " >>"+ str(data)                                     #
+		pass                                                       #
+	c.close()                                                          #
+
+
+
+
+def sendMsg():                                                             #
+	while True:                                                        #
+		data = raw_input("")                                       #
+		if data == "quit()":                                       #
+			for c in lis:                                      #
+				c.close()                                  #
+			os._exit(1)                                        #
+		for c in lis:	                                           #  function used by a thread to send server's msg to all the clients 
+			c.send(data)                                       #    connected to the server
+			pass                                               #
+		pass                                                       #
+
+
+
 def Main():
-	host = "127.0.0.1"
-	port = 3300
+	thread.start_new_thread(sendip,()) #Thread which runs sendip function
+	host = "localhost"
+	port = MYPORT
 	s = socket.socket()
-	s.bind((host,port))
-	s.listen(2)
-	c , addr = s.accept()
-	print "connected :"+ str(addr)
-	thread.start_new_thread(dsp,(c,))
-	thread.start_new_thread(msg,(c,))
+	s.bind(('',port))
+	thread.start_new_thread(sendMsg,()) #Thread which runs sendMsg function
 	while 1:
+		s.listen(3)
+		c , addr = s.accept()
+		lis.append(c)
+		print "connected to :"+ str(addr)
+		thread.start_new_thread(dspMsg,(c,)) #Thread which runs dspMsg function
 		pass	
 	
-	
-if __name__ == '__main__':
+
+if __name__ == '__main__':   
 	Main()
